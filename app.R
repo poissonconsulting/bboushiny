06c5d1e5c7bccde654fae6eb3790454a3c90c480
Seb Dalgarno <seb@poissonconsulting.ca>
2025-06-06 09:36:47 -0700

    final scrub of ECCC licensing

--- app.R
+++ app.R
@@ -1,4 +1,5 @@
-# Copyright 2023 Environment and Climate Change Canada
+# Copyright 2022-2023 Integrated Ecological Research and Poisson Consulting Ltd.
+# Copyright 2024 Province of Alberta
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
